7428b0cab03916cf2dd51ac3ce52ad2ddd9247f8
AldoStalin <37894599+AldoStalin@users.noreply.github.com>
2019-06-20 15:44:34 +0530

    Update a_vowel_consonant.py

--- a_vowel_consonant.py
+++ a_vowel_consonant.py
@@ -1,6 +1,8 @@
 a_char=input()
 a_vowel=['a','e','i','o','u']
-if a_char in a_vowel:
+if a_char.isalpha()==0:
+    print("invalid")
+elif a_char in a_vowel:
     print("Vowel")
 else:
     print("Consonant")
